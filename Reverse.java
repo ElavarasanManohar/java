87088985a1fa60a9a505911bbdbee0d5078aa24c
ElavarasanManohar <40587841+ElavarasanManohar@users.noreply.github.com>
2018-06-26 09:58:30 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -3,13 +3,10 @@ class Reverse
 {
     public static void main(String args[])
     {
-        String n;
         Scanner s=new Scanner(System.in);
         StringBuffer s1=new StringBuffer();
-        n=s.nextLine();
+        String n=s.nextLine();
         s1.append(n);
-        System.out.print(s1.reverse());
-    
-        
+        System.out.print(s1.reverse());   
     }
 }
